dac9a97ed3c5632a1b8757404ba854693f1bc4a6
dasfrischetost <feldoc06@gmail.com>
2024-11-20 21:04:46 +0100

    Final X-Position :0

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
@@ -5,6 +6,7 @@ public class Movement : MonoBehaviour
     Ipatsch inputActions;
     public float Speeeeed;
     public Rigidbody2D fly;
+    public float flying;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,13 +20,16 @@ public class Movement : MonoBehaviour
     {// Vector2 AD wurde nun inputActions.Movement zugewisen
      // und bildet den Output in der klammer ab.
 
-        Vector2 AD = inputActions.Movememt.AD.ReadValue<Vector2>();
-        transform.Translate(AD.x * Speeeeed * Time.deltaTime, 0, 0);
-
-        if (inputActions.Movememt.Jump.triggered)
+        if (fly.linearVelocity.y == 0)
         {
-            fly.AddForce.
-        }
+            Vector2 AD = inputActions.Movememt.AD.ReadValue<Vector2>();
+            transform.Translate(AD.x * Speeeeed * Time.deltaTime, 0, 0);
 
+            if (inputActions.Movememt.Jump.triggered)
+            {
+                fly.linearVelocity = new Vector2 (AD.x * Speeeeed, flying);
+            }
+        }
+    
     }
 }
